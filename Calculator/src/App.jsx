b29e54c91190ffa5f819950702cc33f032921fbe
Pd0ubleU <preawa.ya2548@gmail.com>
2025-03-23 11:53:47 +0700

    Add files via upload

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,14 @@
+import { Calculator } from './component/Calculator.jsx';
+import './App.css'
+
+const App = () => {
+  return (  
+    <div className="Home">
+      <div className="Container">
+          <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
