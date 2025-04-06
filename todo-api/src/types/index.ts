0e9ae4212b977e7d2c0045882805913245596edb
Pd0ubleU <preawa.ya2548@gmail.com>
2025-04-07 02:38:53 +0700

    Add files via upload

---
+++ todo-api/src/types/index.ts
@@ -0,0 +1,17 @@
+export type Todo = {
+    // Implement HERE
+    id: number;         
+    title: string;        
+    completed: boolean;   
+   
+  };
+  
+  export type CreateTodoInput = {
+      title: string;
+      completed?: boolean;
+  };
+  
+  export type UpdateTodoInput = {
+      title?: string;
+      completed?: boolean;
+  };
